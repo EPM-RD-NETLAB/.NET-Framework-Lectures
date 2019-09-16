7067e3b09d82481624a4670735f5e8487e9c918e
Evgeny Yafarkin <evgeny_yafarkin@epam.com>
2019-09-16 14:12:07 +0400

    Добавлена первая лекция

---
+++ NET.C#.01_Введение в .NET Framework 4/NET.C#.01_Введение в .NET Framework 4_DemoFiles/NET.CSharp.01/ConsoleApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            int nextCharacter = Console.Read();
+
+            ConsoleKeyInfo key = Console.ReadKey();
+
+            string line = Console.ReadLine();
+
+            Console.Write("Hello there!");
+
+            Console.WriteLine("Hello there!");
+            Console.ReadLine();
+        }
+    }
+}
